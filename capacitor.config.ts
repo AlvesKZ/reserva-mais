370a7404c3fed686d544fb79026804a5e983b01b
aluno <etecae023@hotmail.com>
2025-08-15 14:26:39 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reserva-mais',
+  webDir: 'www'
+};
+
+export default config;
